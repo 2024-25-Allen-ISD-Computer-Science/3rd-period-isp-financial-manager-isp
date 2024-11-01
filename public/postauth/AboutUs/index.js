33023807eedc695e393399dedf6a48699215516d
Maci Siratt <142333411+maci-siratt@users.noreply.github.com>
2024-11-01 09:55:21 -0500

    open and close sidebar

---
+++ public/postauth/AboutUs/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("navbar1").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+ 
+  function closeNav() {
+    document.getElementById("navbar1").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
