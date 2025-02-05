ee6602db69185627d0c7c2f488219524ee0aac92
Lettuce287 <111583609+Lettuce287@users.noreply.github.com>
2025-02-05 17:57:14 +0000

    moved API key/DB URL to lib/supabase.jsx so that it doesnt kill itself when I run it on different devices

--- financial-manager-isp/src/lib/supabase.jsx
+++ financial-manager-isp/src/lib/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://kdrzejfwglcinzgjbbzu.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkcnplamZ3Z2xjaW56Z2piYnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MDc3MTksImV4cCI6MjA1NDE4MzcxOX0.CxB4WhIe8EUTNbsALF7cKKTmF73ZkaaM0f2qJKK8JX0"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
