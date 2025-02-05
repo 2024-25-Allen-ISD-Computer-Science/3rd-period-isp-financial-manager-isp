151d47da9b58b479f59d1dca7d7a50e92278e752
Lettuce287 <111583609+Lettuce287@users.noreply.github.com>
2025-02-05 18:05:58 +0000

    undid the key movement

--- financial-manager-isp/src/lib/supabase.jsx
+++ financial-manager-isp/src/lib/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://kdrzejfwglcinzgjbbzu.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkcnplamZ3Z2xjaW56Z2piYnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MDc3MTksImV4cCI6MjA1NDE4MzcxOX0.CxB4WhIe8EUTNbsALF7cKKTmF73ZkaaM0f2qJKK8JX0"
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
