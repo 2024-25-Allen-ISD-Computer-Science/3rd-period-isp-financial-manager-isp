7b6e5ac614f219e8996e51f14ddcfd86b2c4d41d
ThomasRoney1 <142262674+ThomasRoney1@users.noreply.github.com>
2025-04-02 23:05:48 +0000

    adding information page to react, should work

--- financial-manager-isp/src/main.jsx
+++ financial-manager-isp/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Inner/Dashboard";
+import Information from "./pages/Inner/Information";
 import Home from "./pages/Home";
 import FourZeroFour from "./pages/FourZeroFour";
 
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/Inner/Dashboard" element={<Dashboard />}/>
+          <Route path="/Inner/Information" element={<Information />} />
           <Route path="*" element={<FourZeroFour />} />
         </Route>
       </Routes>
