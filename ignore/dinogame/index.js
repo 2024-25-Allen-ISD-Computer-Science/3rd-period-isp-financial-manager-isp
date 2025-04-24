a08b2d625559792c05e103c7110632e06f387848
ThomasRoney1 <142262674+ThomasRoney1@users.noreply.github.com>
2025-04-24 12:19:29 -0500

    Update index.js

--- ignore/dinogame/index.js
+++ ignore/dinogame/index.js
@@ -1,16 +1,5 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// extract from chromium source code by @liuwayong
 (function () {
     'use strict';
-    /**
-     * T-Rex runner.
-     * @param {string} outerContainerId Outer containing element id.
-     * @param {Object} opt_config
-     * @constructor
-     * @export
-     */
     function Runner(outerContainerId, opt_config) {
         // Singleton
         if (Runner.instance_) {
